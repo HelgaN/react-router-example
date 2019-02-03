278d4791a0c3e0887b62a692f76215b6e07b2822
HelgaN <ola14@tut.by>
2019-02-03 21:07:19 +0300

    active link

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,15 @@ import React, { Component } from 'react';
 
 import Toolbar from './components/Toolbar';
 import Content from './components/Content';
-import Home from './pages/Home';
 
 class App extends Component {
     render() {
         return (
             <div className="app">
                 <Toolbar />
-                
+
                 <Content>
-                    <Home />
+                    {this.props.children}
                 </Content>
             </div>
         );
--- src/index.js
+++ src/index.js
@@ -1,9 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Router, Route, hashHistory } from 'react-router'; // Router - маршрутизатор; Route - сопост URL с опред.компонентом
 
 import App from './App';
+import About from './pages/About';
+import Books from './pages/Books';
+import NotFound from './pages/NotFound';
+import Home from './pages/Home';
 
 import 'material-components-web/dist/material-components-web.css';
 import './index.css';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <Router history={hashHistory}>
+    <Route component={App}>
+      <Route path="/" component={Home} />                 // либо загрузить компонент IndexRoute
+      <Route path="about" component={About}/>
+      <Route path="books" component={Books}/>
+      <Route path="*" component={NotFound}/>
+    </Route>
+  </Router>,
+   document.getElementById('root'));
